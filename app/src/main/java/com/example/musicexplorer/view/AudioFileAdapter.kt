e6ed651dd576af3f5f3b68b58d0e1c7e2ba45847
AimenArshad <95518409+AimenArshad@users.noreply.github.com>
2025-08-01 12:25:46 +0500

    music explorer app

--- app/src/main/java/com/example/musicexplorer/view/AudioFileAdapter.kt
+++ app/src/main/java/com/example/musicexplorer/view/AudioFileAdapter.kt
@@ -43,14 +43,5 @@ class AudioFileAdapter(
             override fun areContentsTheSame(oldItem: AudioFile, newItem: AudioFile): Boolean =
                 oldItem == newItem
         }
-
-        fun formatDuration(duration: Long): String {
-            val minutes = (duration / 1000) / 60
-            val seconds = (duration / 1000) % 60
-            return String.format("%02d:%02d", minutes, seconds)
-        }
     }
-
-
-
 }
